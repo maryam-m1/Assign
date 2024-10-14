995a219fa628c7e899362f7922906336473a6089
maryam-m1 <maryamrajput0013@gmail.com>
2024-10-14 11:12:45 -0700

    code

---
+++ code.cpp
@@ -0,0 +1,10 @@
+
+using namespace std;
+
+int main() {
+    string statusMessage;
+    cout << "Enter your status message: ";
+    getline(cin, statusMessage);
+    cout << "Status: " << statusMessage << endl;
+    return 0;
+}
